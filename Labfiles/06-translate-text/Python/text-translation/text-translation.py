f8bda71b586e7b8f6421ffdf366202096949d0a8
cb-raghav <raghav@chargebee.com>
2024-01-28 23:16:49 +0530

    Translate text with Azure AI

--- Labfiles/06-translate-text/Python/text-translation/text-translation.py
+++ Labfiles/06-translate-text/Python/text-translation/text-translation.py
@@ -2,10 +2,15 @@ from dotenv import load_dotenv
 import os
 import requests, json
 
+# import namespaces
+from azure.ai.translation.text import *
+from azure.ai.translation.text.models import InputTextItem
+
 def main():
     global translator_endpoint
     global cog_key
     global cog_region
+    global client
 
     try:
         # Get Configuration Settings
@@ -13,6 +18,23 @@ def main():
         cog_key = os.getenv('COG_SERVICE_KEY')
         cog_region = os.getenv('COG_SERVICE_REGION')
         translator_endpoint = 'https://api.cognitive.microsofttranslator.com'
+
+        # Create client using endpoint and key
+        credential = TranslatorCredential(cog_key, cog_region)
+        client = TextTranslationClient(credential)
+
+        # Choose target language for translating reviews
+        languages_response = client.get_languages(scope="translation")
+        print("{} languages supported.".format(len(languages_response.translation)))
+        print("Enter a target language code for translation (for example, 'en'):")
+        target_language = "xx"
+        supported_language = False
+        while supported_language == False:
+            target_language = input()
+            if  target_language in languages_response.translation.keys():
+                supported_language = True
+            else:
+                print("{} is not a supported language.".format(target_language))
 
         # Analyze each text file in the reviews folder
         reviews_folder = 'reviews'
@@ -22,36 +44,24 @@ def main():
             text = open(os.path.join(reviews_folder, file_name), encoding='utf8').read()
             print('\n' + text)
 
-            # Detect the language
-            language = GetLanguage(text)
-            print('Language:',language)
-
             # Translate if not already English
-            if language != 'en':
-                translation = Translate(text, language)
-                print("\nTranslation:\n{}".format(translation))
+            if target_language != 'en':
+                translate(text, target_language)
                 
     except Exception as ex:
         print(ex)
 
-def GetLanguage(text):
-    # Default language is English
-    language = 'en'
-
-    # Use the Azure AI Translator detect function
-
-
-    # Return the language
-    return language
-
-def Translate(text, source_language):
+def translate(input_text, target_language):
     translation = ''
 
     # Use the Azure AI Translator translate function
-
-
-    # Return the translation
-    return translation
+    input_text_elements = [InputTextItem(text=input_text)]
+    translation_response = client.translate(content=input_text_elements, to=[target_language])
+    translation = translation_response[0] if translation_response else None
+    if translation:
+        source_language = translation.detected_language
+        for translated_text in translation.translations:
+            print(f"'{input_text}' was translated from {source_language.language} to {translated_text.to} as '{translated_text.text}'.")
 
 if __name__ == "__main__":
     main()
--- Labfiles/06b-translator-sdk/Python/translate-text/translate.py
+++ Labfiles/06b-translator-sdk/Python/translate-text/translate.py
@@ -2,27 +2,45 @@ from dotenv import load_dotenv
 import os
 
 # import namespaces
-
-
+from azure.ai.translation.text import *
+from azure.ai.translation.text.models import InputTextItem
 
 def main():
     try:
         # Get Configuration Settings
         load_dotenv()
-        translatorRegion = os.getenv('TRANSLATOR_REGION')
-        translatorKey = os.getenv('TRANSLATOR_KEY')
+        translator_region = os.getenv('TRANSLATOR_REGION')
+        translator_key = os.getenv('TRANSLATOR_KEY')
 
         # Create client using endpoint and key
-        
+        credential = TranslatorCredential(translator_key, translator_region)
+        client = TextTranslationClient(credential)
 
+        # Choose target language for translating reviews
+        languages_response = client.get_languages(scope="translation")
+        print("{} languages supported.".format(len(languages_response.translation)))
+        print("Enter a target language code for translation (for example, 'en'):")
+        target_language = "xx"
+        supported_language = False
+        while supported_language == False:
+            target_language = input()
+            if  target_language in languages_response.translation.keys():
+                supported_language = True
+            else:
+                print("{} is not a supported language.".format(target_language))
 
-        ## Choose target language
-
-
-
-        # Translate text
-
-
+         # Translate text
+        input_text = ""
+        while input_text.lower() != "quit":
+            input_text = input("Enter text to translate ('quit' to exit):")
+            if input_text != "quit":
+                input_text_elements = [InputTextItem(text=input_text)]
+                translation_response = client.translate(content=input_text_elements, to=[target_language])
+                translation = translation_response[0] if translation_response else None
+                if translation:
+                    source_language = translation.detected_language
+                    for translated_text in translation.translations:
+                        print(f"'{input_text}' was translated from {source_language.language} to {translated_text.to} as '{translated_text.text}'.")
 
     except Exception as ex:
         print(ex)
